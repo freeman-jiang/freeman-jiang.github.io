891d7f64fa569ee364dcbd680cc4952a4f07d19f
Freeman Jiang <freeman.jiang.ca@gmail.com>
2021-08-21 20:09:41 -0400

    Revamp website

--- README.md
+++
@@ -1,6 +0,0 @@
-# Personal Website (Outdated)
-![image](https://user-images.githubusercontent.com/56516912/120905472-455e6780-c620-11eb-9082-385108d23a47.png)
-![image](https://user-images.githubusercontent.com/56516912/120905634-65daf180-c621-11eb-8a5a-6f0fe65a8b77.png)
-![image](https://user-images.githubusercontent.com/56516912/120905639-70958680-c621-11eb-8bcf-559bb8e38d44.png)
-![image](https://user-images.githubusercontent.com/56516912/120905662-a0448e80-c621-11eb-87eb-ef3977bad36b.png)
-![image](https://user-images.githubusercontent.com/56516912/120905654-8014cf80-c621-11eb-981f-027604621245.png)
--- js/custom.js
+++ js/custom.js
@@ -1,169 +1,114 @@
-/*-----------------------------------------------------------
-* Template Name    : Kerri | Responsive Bootstrap 4 Personal Template
-* Author           : SRBThemes
-* Version          : 1.0.0
-* Created          : March 2018
-* File Description : Main Js file of the template
-*------------------------------------------------------------
-*/
+!(function ($) {
+  'use strict'
 
-! function($) {
-    "use strict";
+  var App = function () {}
 
-    var KerriApp = function() {};
-
+  //PreLoader
+  ;(App.prototype.initPreLoader = function () {
+    $('#status').fadeOut()
+    $('#preloader').delay(350).fadeOut('slow')
+    $('body').delay(350).css({
+      overflow: 'visible',
+    })
+  }),
     //scroll
-    KerriApp.prototype.initStickyMenu = function() {
-        $(window).on('scroll',function() {
-            var scroll = $(window).scrollTop();
-
-            if (scroll >= 50) {
-                $(".sticky").addClass("stickyadd");
-            } else {
-                $(".sticky").removeClass("stickyadd");
-            }
-        });
-    },
-
-    // CollapseHide
-    KerriApp.prototype.initCollapseHide = function() {
-        $(document).on('click', '.navbar-collapse.show', function(e) {
-            if ($(e.target).is('a')) {
-                $(this).collapse('hide');
-            }
-        });
-    },
-
-    //Smooth
-    KerriApp.prototype.initSmoothLink = function() {
-        $('.navbar-nav a, .scroll_down a').on('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 0
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    },
-
-    //Scrollspy
-    KerriApp.prototype.initScrollspy = function() {
-        $("#navbarCollapse").scrollspy({
-            offset:20
-        });
-    },
-
-    //Typed
-    KerriApp.prototype.initTextType = function() {
-        $(".element").each(function() {
-            var $this = $(this);
-            $this.typed({
-                strings: $this.attr('data-elements').split(','),
-                typeSpeed: 100,
-                backDelay: 3000
-            });
-        });
-    },
-
+    (App.prototype.initStickyMenu = function () {
+      var navbar = document.querySelector('nav')
+      window.onscroll = function () {
+        // pageYOffset or scrollY
+        if (window.pageYOffset > 200) {
+          navbar.classList.add('stickyadd')
+        } else {
+          navbar.classList.remove('stickyadd')
+        }
+      }
+    }),
+    // scrollspy
+    (App.prototype.initScrollspy = function () {
+      var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: '#main_nav',
+        offset: 70,
+      })
+    }),
     //Work
-    KerriApp.prototype.initWork = function() {
-        $(window).on('load', function () {
-            var $container = $('.work-filter');
-            var $filter = $('#menu-filter');
-            $container.isotope({
-                filter: '*',
-                layoutMode: 'masonry',
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear'
-                }
-            });
+    (App.prototype.initWork = function () {
+      $(window).on('load', function () {
+        var $container = $('.work-filter')
+        var $filter = $('#menu-filter')
+        $container.isotope({
+          filter: '*',
+          layoutMode: 'fitRows',
+          animationOptions: {
+            duration: 750,
+            easing: 'linear',
+          },
+        })
 
-            $filter.find('a').on("click",function() {
-                var selector = $(this).attr('data-filter');
-                $filter.find('a').removeClass('active');
-                $(this).addClass('active');
-                $container.isotope({
-                    filter: selector,
-                    animationOptions: {
-                        animationDuration: 750,
-                        easing: 'linear',
-                        queue: false,
-                    }
-                });
-                return false;
-            });
-        });
-    },
-
+        $filter.find('a').on('click', function () {
+          var selector = $(this).attr('data-filter')
+          $filter.find('a').removeClass('active')
+          $(this).addClass('active')
+          $container.isotope({
+            filter: selector,
+            animationOptions: {
+              animationDuration: 750,
+              easing: 'linear',
+              queue: false,
+            },
+          })
+          return false
+        })
+      })
+    }),
     //Magnificpop
-    KerriApp.prototype.initMagnificPopup = function() {
-        $('.img-zoom').magnificPopup({
-            type: 'image',
-            closeOnContentClick: true,
-            mainClass: 'mfp-fade',
-            gallery: {
-                enabled: true,
-                navigateByImgClick: true,
-                preload: [0, 1]
-            }
-        });
-    },
-
-    //Client
-    KerriApp.prototype.initTestimonial = function() {
-        $("#owl-demo").owlCarousel({
-            autoPlay: 7000,
-            stopOnHover: true,
-            navigation: false,
-            paginationSpeed: 1000,
-            goToFirstSpeed: 2000,
-            singleItem: true,
-            autoHeight: true,
-        });
-    },
-
-    //PRELOADER
-    KerriApp.prototype.initPreloader = function() {
-        $('#status').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
-    },
-
+    (App.prototype.initMagnificPopup = function () {
+      $('.img-zoom').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-fade',
+        gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0, 1],
+        },
+      })
+    }),
     // BACK TO TOP
-    KerriApp.prototype.initBackToTop = function() {
-        $(window).on('scroll',function(){
-            if ($(this).scrollTop() > 100) {
-                $('.back_top').fadeIn();
-            } else {
-                $('.back_top').fadeOut();
-            }
-        }); 
-        $('.back_top').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 1000);
-            return false;
-        });
-    },
-
-    KerriApp.prototype.init = function() {
-        this.initStickyMenu();
-        this.initSmoothLink();
-        this.initCollapseHide();
-        this.initScrollspy();
-        this.initTextType();
-        this.initWork();
-        this.initMagnificPopup();
-        this.initPreloader();
-        this.initTestimonial();
-        this.initBackToTop();
-    },
+    (App.prototype.initBackToTop = function () {
+      $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 100) {
+          $('.back_top').fadeIn()
+        } else {
+          $('.back_top').fadeOut()
+        }
+      })
+      $('.back_top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000)
+        return false
+      })
+    }),
+    (App.prototype.init = function () {
+      this.initPreLoader()
+      this.initStickyMenu()
+      this.initScrollspy()
+      this.initWork()
+      this.initMagnificPopup()
+      this.initBackToTop()
+    }),
     //init
-    $.KerriApp = new KerriApp, $.KerriApp.Constructor = KerriApp
-}(window.jQuery),
+    ($.App = new App()),
+    ($.App.Constructor = App)
+})(window.jQuery),
+  //initializing
+  (function ($) {
+    'use strict'
+    $.App.init()
+  })(window.jQuery)
 
-//initializing
-function($) {
-    "use strict";
-    $.KerriApp.init();
-}(window.jQuery);
+// Randomize Background
+// const home = document.querySelector('#home')
+// const flipCoin = Math.floor(Math.random() * 2)
+// console.log(flipCoin)
+// home.style.backgroundImage = flipCoin
+//   ? "url('images/header-bg.jpg')"
+//   : "url('images/header-bg2.jpg')"
